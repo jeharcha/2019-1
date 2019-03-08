a11ecd20f2f7e9b19166b2e2ca61569e06cf5efe
Vicente Errázuriz <verrazuriz@uc.cl>
2019-03-08 14:07:03 -0300

    Taller 0

---
+++ Talleres/Taller 0/src/act0/main.c
@@ -0,0 +1,29 @@
+/****************************************************************************/
+/*                            Acto 0: Tipos básicos                         */
+/****************************************************************************/
+
+/** Módulo estándar de Input y Output */
+#include <stdio.h>
+
+/** Esta función es lo que se llama al ejecutar tu programa */
+int main()
+{
+  /* Imprime el mensaje en la consola */
+  printf("Bienvenido al Acto 0: Tipos de datos\n");
+
+  // Números enteros
+
+  // Números reales
+
+  // Booleans
+
+  // Caracteres
+
+  ////////////////////////////////////////////////////////////////////////////
+  //////////////////////  POLICE LINE DO NOT CROSS  //////////////////////////
+  ////////////////////////////////////////////////////////////////////////////
+
+  // Strings
+
+  return 0;
+}
---
+++ Talleres/Taller 0/src/act2/main.c
@@ -0,0 +1,23 @@
+/****************************************************************************/
+/*                              Acto 2: Punteros                            */
+/****************************************************************************/
+
+/** Módulo estándar de Input y Output */
+#include <stdio.h>
+/** Módulo estándar de números enteros */
+#include <stdint.h>
+/** Módulo estándar de valores booleanos */
+#include <stdbool.h>
+
+/** Esta función es lo que se llama al ejecutar tu programa */
+int main()
+{
+  /* Imprime el mensaje en la consola */
+  printf("Bienvenido al Acto 2: Punteros\n");
+
+  ////////////////////////////////////////////////////////////////////////////
+  //////////////////////  POLICE LINE DO NOT CROSS  //////////////////////////
+  ////////////////////////////////////////////////////////////////////////////
+
+  return 0;
+}
